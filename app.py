ac2dc2467663e0db74f7a22c0bde7b7812b0e8f0
Michael <m.anderjaska@gmail.com>
2017-03-26 08:44:42 -0400

    fixed chatbot issue

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask
 from flask import render_template,request, jsonify
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 
-import io
 from chatterbot import ChatBot
 
 import trainer
 import os
+from json import dumps
+
+import threading
+import requests
+
+
+
 
 app = Flask(__name__)
 
@@ -37,17 +45,36 @@ def handleSpeech():
 		#trainer.raw_text = response
 		#execfile("trainer.py")
 		# LSTM.raw_text = text // LSTM.main()<< could do this to use the neural net in the future
-		recc = response
-		print type(recc)
-		print recc
+		recc = response.serialize()
 
-		return jsonify({'recc': recc})
+		print recc['text'] 
 
+		
+
+		
+
+		return jsonify({'recc': recc['text']})
+
+
+
+
+def set_interval(func, sec):
+	print 'starting interval'
+  def func_wrapper():
+      set_interval(func, sec)
+      func()
+  t = threading.Timer(sec, func_wrapper)
+  t.start()
+  return t
+
+def keep_app_alive():
+	requests.get('http://tipofthetongue.herokuapp.com/')
 
 
 
 
 if __name__ == '__main__':
+		set_interval(keep_app_alive, 1700)
     port = int(os.environ.get("PORT", 5000))
     http_server = HTTPServer(WSGIContainer(app))
     http_server.listen(port)
--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from chatterbot import ChatBot
 
 chatbot = ChatBot(
